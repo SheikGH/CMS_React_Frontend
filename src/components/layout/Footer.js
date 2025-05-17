0cd77dc001be7e34dc78e8d5108c4c3c46146520
SheikGH <sheik.ak2006@gmail.com>
2025-05-17 23:59:41 +0400

    ChangeCode1

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer className="py-1 bg-dark">
-            <p className="text-center text-white mt-1 ">
+            <p className="text-center text-white mt-1">
                 Customer Management System - 2023-2024, All Rights Reserved
             </p>
         </footer>
